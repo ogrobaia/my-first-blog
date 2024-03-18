45803d91b91c55369f0b1186be6fd77b01042438
ogrobaia <daniel.tar.gz.nagumo@gmail.com>
2024-03-18 14:24:41 -0300

    Correção do erro blog/post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
